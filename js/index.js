f6fc75088f23c741829dfd691a1ecb3ede4ba3f3
Badal <badalhasanbd5@gmail.com>
2024-09-22 10:55:09 +0600

    compaleted

--- js/index.js
+++ js/index.js
@@ -1,11 +1,34 @@
-
+function getInputValue (id){
+    return parseFloat(document.getElementById(id).value)
+}
 // calculator Button
-
+let count =0;
 const calculateButton = document.getElementById('calculate').addEventListener('click', function(){
+    count +=1;
     const income = parseFloat(document.getElementById('income').value);
     const software =parseFloat(document.getElementById('software').value);
     const courses =parseFloat(document.getElementById('courses').value);
     const internet =parseFloat(document.getElementById('internet').value);
+    // validaton cheaking
+    if(income<=0 || isNaN(income)){
+        document.getElementById('income-error').classList.remove('hidden')
+        return ;
+    }
+
+    if(software<=0 || isNaN(software)){
+        document.getElementById('software-error').classList.remove('hidden')
+        return ;
+    }
+
+    if(courses<=0 || isNaN(courses)){
+        document.getElementById('courses-error').classList.remove('hidden')
+        return ;
+    }
+
+    if(internet<=0 || isNaN(internet)){
+        document.getElementById('internet-error').classList.remove('hidden')
+        return ;
+    }
 
     const totalExpesses = software+courses+internet
     const blance = income-totalExpesses;
@@ -17,6 +40,20 @@ const calculateButton = document.getElementById('calculate').addEventListener('c
 
     const result = document.getElementById('results')
     result.classList.remove('hidden')
+    // const historySectionel =document.getElementById('history-section')
+    // historySectionel.classList.remove('hidden')
+    const historyList = document.getElementById('history-list')
+    const div = document.createElement('div')
+    div.innerHTML=`<div class ="bg-white p-3 rounded-md border-l-2 border-indigo-500">
+    <p class = "text-xs font-blod text-gray-500">Serial: ${count}</p>
+    <p class = "text-xs text-gray-500">Income: ${income.toFixed(2)}</p>
+    <p class = "text-xs text-gray-500">Expesses: ${totalExpesses.toFixed(2)}</p>
+    <p class = "text-xs text-gray-500">Blance: ${blance.toFixed(2)}</p>
+    </div>
+   
+
+    `
+    historyList.insertBefore(div, historyList.firstChild)
 })
 
 // Savininigs Calculator
